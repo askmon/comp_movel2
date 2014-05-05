e0d0e6f4173273144f7da222efc08166d5750904
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-05-05 15:24:01 -0300

    Adicionei a classe Geometry para usar nos Quads

--- BrickBreaker/src/usp/ime/movel/brickbreaker/Quad.java
+++ BrickBreaker/src/usp/ime/movel/brickbreaker/Quad.java
@@ -6,9 +6,11 @@ import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import usp.ime.movel.brickbreaker.graphics.Geometry;
+
 class Quad {
-    private float posX = 0.0f;
-    private float posY = 0.0f;
+
+    private Geometry geom;
 
     private FloatBuffer vertexBuffer;
     private FloatBuffer colorBuffer;
@@ -24,6 +26,8 @@ class Quad {
     
     
     public Quad(float r, float g, float b) {
+    	this.geom = new Geometry();
+    	
         ByteBuffer vbb = ByteBuffer.allocateDirect( vertices.length * FLOAT_SIZE_BYTES );
         vbb.order( ByteOrder.nativeOrder() );
         vertexBuffer = vbb.asFloatBuffer();
@@ -46,8 +50,7 @@ class Quad {
     
     
     public void setPosition( float x, float y ) {
-        this.posX = x;
-        this.posY = y;
+    	this.geom.setPosition(x, y);
     }
 
 
@@ -55,8 +58,8 @@ class Quad {
         gl.glMatrixMode( GL10.GL_MODELVIEW );
         gl.glPushMatrix();
         gl.glLoadIdentity();
-        gl.glTranslatef( posX, posY, 0.0f );
-        gl.glScalef( 0.5f, 0.5f, 0.5f );
+        gl.glTranslatef( this.geom.getX(), this.geom.getY(), 0.0f );
+        gl.glScalef( this.geom.getWidth(), this.geom.getHeight(), 0.5f );
 
         gl.glEnableClientState( GL10.GL_VERTEX_ARRAY );
         gl.glEnableClientState( GL10.GL_COLOR_ARRAY );
---
+++ BrickBreaker/src/usp/ime/movel/brickbreaker/graphics/Geometry.java
@@ -0,0 +1,47 @@
+package usp.ime.movel.brickbreaker.graphics;
+
+public class Geometry {
+
+	private float x;
+	private float y;
+	private float width;
+	private float height;
+
+	public Geometry(float x, float y, float width, float height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Geometry() {
+		this(0.0f, 0.0f, 0.1f, 0.1f);
+	}
+	
+	public float getX() {
+		return this.x;
+	}
+	
+	public float getY() {
+		return this.y;
+	}
+	
+	public void setPosition(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public float getWidth() {
+		return this.width;
+	}
+	
+	public float getHeight() {
+		return this.height;
+	}
+	
+	public void setShape(float width, float height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+}
