cb2521670d9b67b218349e7aa0f2169aeca450aa
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-05-14 19:41:49 -0300

    Efeito e impacto quando tijolos são atingidos e não morrem

--- BrickBreaker/src/usp/ime/movel/brickbreaker/game/BrickEntity.java
+++ BrickBreaker/src/usp/ime/movel/brickbreaker/game/BrickEntity.java
@@ -14,6 +14,9 @@ public class BrickEntity extends Entity {
 	private static int ingame_count = 0;
 	private static int current_texture;
 	private int hp, max_hp;
+	private float impact_x, impact_y;
+	private float center_x, center_y;
+	private int impact_timer;
 	private DyingBrickEntity dying_effect;
 
 	public BrickEntity(float x, float y) {
@@ -22,6 +25,11 @@ public class BrickEntity extends Entity {
 		this.view = null;
 		this.destroyed = false;
 		this.dying_effect = null;
+		this.center_x = x;
+		this.center_y = y;
+		this.impact_x = 0.0f;
+		this.impact_y = 0.0f;
+		this.impact_timer = 0;
 	}
 
 	private void setHp() {
@@ -60,7 +68,10 @@ public class BrickEntity extends Entity {
 
 	@Override
 	public void onUpdate(TouchSurfaceView view) {
-		// TODO Auto-generated method stub
+		Geometry geom = getSprite().getGeometry();
+		float oscilation = (float) (0.025*impact_timer/15.0f*Math.cos((15-impact_timer)/5.0f*2.0f*Math.PI));
+		geom.setPosition(center_x + oscilation*impact_x, center_y + oscilation*impact_y);
+		impact_timer = Math.max(0, impact_timer-1);
 	}
 
 	public void inflictDamage(int ballDamage, float impact_x, float impact_y) {
@@ -72,8 +83,12 @@ public class BrickEntity extends Entity {
 			dying_effect = new DyingBrickEntity(getSprite().getGeometry()
 					.getX(), getSprite().getGeometry().getY(), impact_x,
 					impact_y, current_texture);
+		} else {
+			this.impact_timer = 15;
+			this.impact_x = impact_x;
+			this.impact_y = impact_y;
+			getSprite().setColor(1.0f, 1.0f*hp/max_hp, 1.0f*hp/max_hp, 1.0f);
 		}
-		getSprite().setColor(1.0f, 1.0f*hp/max_hp, 1.0f*hp/max_hp, 1.0f);
 	}
 
 	@Override
