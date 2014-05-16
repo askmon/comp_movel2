a4480db5d30e215d91fb1c0db2c34d0dac3d60e7
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-05-16 11:06:46 -0300

    Adicionei SpawnerEntity e NullSprite

---
+++ BrickBreaker/src/usp/ime/movel/brickbreaker/game/SpawnerEntity.java
@@ -0,0 +1,18 @@
+package usp.ime.movel.brickbreaker.game;
+
+import usp.ime.movel.brickbreaker.graphics.NullSprite;
+import usp.ime.movel.brickbreaker.graphics.TouchSurfaceView;
+
+public class SpawnerEntity extends Entity {
+
+	public SpawnerEntity() {
+		super(new NullSprite());
+	}
+
+	@Override
+	public void onUpdate(TouchSurfaceView view) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
