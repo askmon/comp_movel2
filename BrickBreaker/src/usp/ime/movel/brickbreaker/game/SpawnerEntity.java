91d03415a835fd30e6348305562de62a317ac2d5
Diego Araújo <diegoamc90@gmail.com>
2014-05-18 16:02:47 -0300

    Tirei um trecho de código que não era usado

--- BrickBreaker/src/usp/ime/movel/brickbreaker/game/SpawnerEntity.java
+++ BrickBreaker/src/usp/ime/movel/brickbreaker/game/SpawnerEntity.java
@@ -6,7 +6,6 @@ import usp.ime.movel.brickbreaker.graphics.TouchSurfaceView;
 public class SpawnerEntity extends Entity {
 
 	private int tempo = 0;
-	private int powerup = 0;
 
 	public SpawnerEntity() {
 		super(new NullSprite());
@@ -18,16 +17,6 @@ public class SpawnerEntity extends Entity {
 		if(tempo >= 300){
 			tempo = 0;
 			view.addEntity(new PowerEntity(view.getSpaceWidth()));
-		}
-		switch(powerup){
-		case 0:
-			break;
-		case 1:
-			
-			break;
-		case 2:
-			
-			break;
 		}
 	}
 
